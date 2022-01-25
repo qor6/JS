cd6b1d84dc64a6e291367850c9a6552c147c9888
qor6 <87318054+qor6@users.noreply.github.com>
2022-01-25 13:36:42 +0900

    variable
    JS는 이름을 카멜,
    py는 이름을 스네이크 표기로

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-console.log("fdlkj")
-console.log(123)
+console.log("fdlkj");
+console.log(123);
+
+console.log(5+2);
+console.log(5*2);
+console.log(5/2);
+
+const a = 5;
+const b = 2;
+const myName = "sumin";
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+
+console.log("Hello " + "Sumin");
+console.log("Hello " + myName);
+
